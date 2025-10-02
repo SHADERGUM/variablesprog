03f55065cf80eca1bcf179205d749dd12801ccff
Marcos <cernadascurrante9399@hotmail.com>
2025-10-02 13:47:59 +0200

    Avance Historia Araña

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Iniciando programa");
-        int x =12;
+        //TRABAJO DE LA ARAÑA//
+
+        Scanner lectorLineaComandos = new Scanner(System.in);
+        String nombre="makelele";
+        String color="Negro";
+        int tamanocm=9;
+        boolean escapoConVida=false;
+
+        System.out.println("\nEra un día cualquiera en una intensa clase de programación,");
+        System.out.println("todo iba bien y nada importante parecía ocurrir hasta que apareció "+(nombre));
+        System.out.println("una de las más temidas arañas del barrio Matograndés");
+        System.out.println("\nEscoge que hacer:");
+        System.out.println("1) Salir corriendo de clase");
+        System.out.println("2) Esperar y quedarse en sigilo para ver que hacer");
+        lectorLineaComandos.nextLine();
+
+        if ("Salir corriendo de clase"){
+        }
+
+
+
     }
 
     }
